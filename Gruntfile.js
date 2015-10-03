eef6bf0a996fb22ab10a79036617848ed755ccc5
Alexey <maviroot@gmail.com>
2015-10-03 21:05:11 +0300

    index.html+css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,117 @@
-'use strict';
-
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
 
-  var config = {
-    pkg: grunt.file.readJSON('package.json')
-  };
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-githooks');
+  grunt.loadNpmTasks('grunt-lintspaces');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  config = require('./.gosha')(grunt, config);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-  grunt.initConfig(config);
+    less: {
+      style: {
+        files: {
+          'css/style.css': 'less/style.less'
+        }
+      }
+    },
+    
+    watch: {
+      styles: {
+        files: ['less/**/*.less'],
+        tasks: ['less','notify:less'],
+        options: {
+            spawn: false,
+            livereload: true
+        }
+      },
+      livereload: {
+        files: ['*.html'],
+        tasks: ['notify:html'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+
+
+    notify: {
+      less: {
+        options: {
+          title: 'Ура!',  // optional
+          message: 'LESS героически скомпилирован', //required
+        }
+      },
+      html: {
+        options: {
+          title: 'Ура-Ура!',  // optional
+          message: 'HTML обновлен!', //required
+        }
+      }
+    },
+
+    sass: {
+      style: {
+        files: {
+          'css/style.css': 'sass/style.scss'
+        }
+      }
+    },
+
+    lintspaces: {
+      test: {
+        src: [
+          '*.html',
+          'js/*.js',
+          'less/*.less',
+          'sass/*.sass'
+        ],
+        options: {
+          editorconfig: '.editorconfig'
+        }
+      }
+    },
+
+    githooks: {
+      test: {
+        'pre-commit': 'lintspaces:test',
+      }
+    },
+
+    copy: {
+      gosha: {
+        files: [{
+          expand: true,
+          src: [
+            '*.html',
+            'css/**',
+            'img/**',
+            'js/**'
+          ],
+          dest: 'gosha',
+        }]
+      }
+    },
+
+    clean: {
+      gosha: [
+        'gosha/img/README',
+        'gosha/js/README'
+      ]
+    }
+  });
+
+  grunt.registerTask('test', ['lintspaces:test']);
+
+  if (grunt.file.exists(__dirname, 'less', 'style.less')) {
+    grunt.registerTask('gosha', ['less:style', 'copy:gosha', 'clean:gosha']);
+  } else if (grunt.file.exists(__dirname, 'sass', 'style.scss')) {
+    grunt.registerTask('gosha', ['sass:style', 'copy:gosha', 'clean:gosha']);
+  } else {
+    grunt.registerTask('gosha', ['copy:gosha', 'clean:gosha']);
+  }
 };
